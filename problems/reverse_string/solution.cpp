304ee8e73dee00c799d9679144870252cef73ece
Omar Hisham <52583910+OmarHisham99@users.noreply.github.com>
2022-05-09 20:39:37 +0000

    Sync LeetCode submission - Reverse String (cpp)

---
+++ problems/reverse_string/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
+        int left = 0, right = s.size()-1; 
+        while(left<right){
+            swap(s[left],s[right]); 
+            left++; 
+            right--; 
+        }
+    }
+};
