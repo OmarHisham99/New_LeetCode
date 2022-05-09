e63f7a1ba83365eb71f2918b6b530984915b33fa
Omar Hisham <52583910+OmarHisham99@users.noreply.github.com>
2022-05-09 20:26:43 +0000

    Sync LeetCode submission - Two Sum (cpp)

--- problems/two_sum/solution.cpp
+++ problems/two_sum/solution.cpp
@@ -1,16 +1,22 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> v1 ; 
-        for(int i= 0 ; i< nums.size();i++){
-            for(int j= i+1;j<nums.size();j++){
-                if(nums.at(i)+nums.at(j)==target){
-                    v1.push_back(i);
-                    v1.push_back(j);
-                    break;
-                }
-            }
-        }
-        return v1; 
+      ios_base::sync_with_stdio(0);
+     cin.tie(0);
+     cout.tie(0);
+     unordered_map<int,int> m;
+     vector<int> index;
+     for (int i = 0; i < nums.size(); i++) {
+    
+          if (m.find(target - nums[i]) != m.end()) {
+               index.push_back(i);
+               index.push_back(m[target - nums[i]]);
+               return index; 
+          }
+          else {
+               m[nums[i]] = i; 
+          }
+     }
+     return index;
     }
 };
