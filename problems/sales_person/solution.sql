4cfc629d4c23ea1aaa92f24ac4cdb0d5484b1b46
Omar Hisham <52583910+OmarHisham99@users.noreply.github.com>
2022-08-28 01:30:03 +0000

    Sync LeetCode submission - Sales Person (mysql)

---
+++ problems/sales_person/solution.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+select sp.name from SalesPerson sp 
+where sp.sales_id not in (
+    select o.sales_id from orders o
+    left join company c on o.com_id = c.com_id
+    where c.name = 'RED'
+)
