96254962c893269ae24b739b9c50f1bedd8ee5ce
Omar Hisham <52583910+OmarHisham99@users.noreply.github.com>
2022-09-06 00:25:25 +0000

    Sync LeetCode submission - Binary Tree Pruning (cpp)

---
+++ problems/binary_tree_pruning/solution.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* dfs(TreeNode* root){
+        if(root == NULL){
+            return NULL; 
+        }
+        root->left = dfs(root->left); 
+        root->right = dfs(root->right); 
+        if(root->val ==0 && root->left ==NULL && root->right == NULL ){
+            return NULL; 
+        }
+        return root; 
+    }
+    TreeNode* pruneTree(TreeNode* root) {
+        return dfs(root); 
+    }
+};
