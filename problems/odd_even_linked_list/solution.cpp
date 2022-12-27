18d619c799c5fbfc909447c1ffa2a1e402d111f3
Omar Hisham <52583910+OmarHisham99@users.noreply.github.com>
2022-12-27 17:09:17 +0000

    Sync LeetCode submission - Odd Even Linked List (cpp)

---
+++ problems/odd_even_linked_list/solution.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        if(!head || !head->next || !head->next->next){
+            return head; 
+        }
+        ListNode* odd = head ,*even = odd->next; 
+        ListNode* hodd = head , *heven = odd->next ; 
+
+        while(even && even->next){
+            odd -> next = odd->next->next; 
+            even -> next = even->next->next; 
+            odd = odd->next; 
+            even = even->next; 
+        }
+        odd->next = heven; 
+        return hodd; 
+        
+    }
+};
