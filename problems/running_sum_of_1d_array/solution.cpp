cb49ca398b9fcd4cb008093053a8ac6555adfe09
Omar Hisham <52583910+OmarHisham99@users.noreply.github.com>
2023-01-01 14:08:28 +0000

    Sync LeetCode submission - Running Sum of 1d Array (cpp)

--- problems/running_sum_of_1d_array/solution.cpp
+++ problems/running_sum_of_1d_array/solution.cpp
@@ -1,12 +1,9 @@
 class Solution {
 public:
     vector<int> runningSum(vector<int>& nums) {
-        vector<int> dp ; 
-        dp.push_back(nums[0]); 
-        for(int i= 1 ;i<nums.size();i++) {
-            dp.push_back(nums[i]+dp[i-1]); 
+        for(int i=1;i<nums.size();i++){
+            nums[i]+=nums[i-1]; 
         }
-        
-        return dp; 
+        return nums; 
     }
 };
